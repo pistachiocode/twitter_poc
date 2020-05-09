3ceba02f054b0129a497a4eec5d312cd073284b1
BSS <beatriznorrase@gmail.com>
2020-05-09 20:31:39 +0200

    move constants

--- src/twitter_conf.py
+++ src/twitter_conf.py
@@ -1,11 +1,12 @@
-# ----------- 
+import json
+
+# -----------
 # CONSTANTS
 # -----------
 
 #
 # Folder used to store extracted tweets
 #
-
 DATA_FOLDER = "../data/"
 
 #
@@ -19,7 +20,6 @@ RESULTS_FOLDER = "../results/"
 
 LOGS_FOLDER = "../logs/"
 
-
 #
 # Formats
 #
@@ -27,13 +27,22 @@ DATE_FORMAT = '%Y-%m-%d'
 TIME_FORMAT = '%H:%M:%S'
 DATETIME_FORMAT = DATE_FORMAT+' '+TIME_FORMAT
 
-
 #
 # Search options
 #
 SEARCH_CURRENT_MAX_ID = -1
-SEARCH_ATTEMPS        = 3
-SEARCH_SAVE_TWEETS    = True
-SEARCH_KEYWORD_LIST   = ["EspañaExiste", "PSOE", "Ciudadanos", "UnidasPodemos", 
-                         "Podemos", "VOX", "ERC", "Bildu", "TeruelExiste", "CoaliciónCanaria"]
-SEARCH_KEYWORD        = "#SesiondeInvestidura"
+SEARCH_ATTEMPS = 3
+SEARCH_SAVE_TWEETS = True
+SEARCH_KEYWORD_LIST = ["VOX"]
+SEARCH_KEYWORD = "#SesiondeInvestidura"
+
+
+with open("../config.json") as fconfig:
+    config = json.load(fconfig)
+
+CONSUMER_KEY = config["CONSUMER_KEY"]
+CONSUMER_SECRET = config["CONSUMER_SECRET"]
+ACCESS_TOKEN = config["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = config["ACCESS_TOKEN_SECRET"]
+
+DATA_FOLDER = "../data/"
